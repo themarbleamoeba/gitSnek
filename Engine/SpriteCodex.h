6e187c0ac784859902bc0f945a6eabeaf484f567
Trevor Thomas <tlthomas85@gmail.com>
2019-02-20 10:34:34 -0500

    Add project files.

---
+++ Engine/SpriteCodex.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Graphics.h"
+
+class SpriteCodex
+{
+public:
+	static void DrawGameOver(int x, int y, Graphics& gfx);
+	static void DrawTitle(int x, int y, Graphics& gfx);
+};
